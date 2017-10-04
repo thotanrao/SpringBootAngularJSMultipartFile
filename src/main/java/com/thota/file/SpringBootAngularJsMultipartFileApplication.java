cde69ddfa23c67931bb08da8f9f765a03150cf1d
thotanrao <thotanrao@gmail.com>
2017-10-03 21:35:28 -0400

    uploading a file using spring boot and Storage service

---
+++ src/main/java/com/thota/file/SpringBootAngularJsMultipartFileApplication.java
@@ -0,0 +1,27 @@
+package com.thota.file;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.thota.file.storage.StorageService;
+
+@SpringBootApplication
+public class SpringBootAngularJsMultipartFileApplication implements CommandLineRunner {
+
+	@Resource
+	StorageService storageService;
+    
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootAngularJsMultipartFileApplication.class, args);
+    }
+    
+	@Override
+	public void run(String... args) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+	}
+
+}
